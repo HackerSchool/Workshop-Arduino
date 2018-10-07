b14e07f0b831cc8bf8af2e615e0405dc38f43cab
Pedro Direita <pedro.direita@gmail.com>
2018-10-07 02:39:30 +0100

    Declaração de define's e nomes de variáveis mais pedagogicos (step5 - step6)
    - Incentivar boas praticas de programação usando defines
    - Uso da variavel buzzer em vez de led em exercicios onde o buzzer é usado

--- step5a/step5a.ino
+++ step5a/step5a.ino
@@ -1,26 +1,29 @@
+#define BOTAO 2
+#define BUZZER 3
+#define LDR 0
+
 bool state = false, curr = false, prev = false;
 
 void setup() {
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,OUTPUT);
+  pinMode(BOTAO,INPUT_PULLUP);
+  pinMode(BUZZER,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   
-  int sensor, led, serial_in;
-  sensor = analogRead(0);
-  led = map(sensor,0,1023,500,5000);
+  int sensor, buzzer, serial_in;
+  sensor = analogRead(LDR);
+  buzzer = map(sensor,0,1023,500,5000);
 
-  curr = digitalRead(2);
+  curr = digitalRead(BOTAO);
   
   if(curr == LOW && prev == HIGH){
     state = !state;
   }
 
-  
   if(!state){
-    Serial.println(led);
+    Serial.println(buzzer);
   }else{
     Serial.println(0);
   }
@@ -28,11 +31,10 @@ void loop() {
   if(Serial.available()!=0){
     serial_in = Serial.parseInt();
     if(serial_in==0){
-      noTone(3);
+      noTone(BUZZER);
     }else{
-      tone(3,serial_in);
-    }
-    
+      tone(BUZZER,serial_in);
+    }    
   }
 
   prev = curr;
--- step6a/step6a.ino
+++ step6a/step6a.ino
@@ -1,20 +1,23 @@
+#define BOTAO 2
+#define BUZZER 3
+#define LDR 0
 
 volatile bool play = false; 
 
 void setup() {
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,OUTPUT);
+  pinMode(BOTAO,INPUT_PULLUP);
+  pinMode(BUZZER,OUTPUT);
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(2), interrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BOTAO), interrupt, FALLING);
 }
 
 void loop() {
-  int sensor, led, serial_in;
-  sensor = analogRead(0);
-  led = map(sensor,0,1023,500,5000);
+  int sensor, buzzer, serial_in;
+  sensor = analogRead(LDR);
+  buzzer = map(sensor,0,1023,500,5000);
 
   if(!play){
-    Serial.println(led);
+    Serial.println(buzzer);
   }else{
     Serial.println(0);
   }
@@ -22,11 +25,10 @@ void loop() {
   if(Serial.available()!=0){
     serial_in = Serial.parseInt();
     if(serial_in==0){
-      noTone(3);
+      noTone(BUZZER);
     }else{
-      tone(3,serial_in);
-    }
-    
+      tone(BUZZER,serial_in);
+    } 
   }
 }
 
